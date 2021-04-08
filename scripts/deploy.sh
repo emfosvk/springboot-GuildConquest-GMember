b5f787f88de8d5767e7e9dae23c48e39aa4eac39
Rosa <emfosvk@gmail.com>
2021-04-08 21:02:38 +0900

    AWS 자동배포 설정 추가.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,14 +11,15 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> [01 - E N D] Build File Copy"
 echo "> [02 - START] Find Service PID activating now"
 
-CURRENT_PID= $(pgrep -fl springboot-GuildConquest-GMember | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-GuildConquest-GMember | grep jar | awk '{print $1}')
+
 echo "> 현재기동중인 PID : $CURRENT_PID"
 
 echo "> [02 - E N D] Build Project"
 echo "> [03 - START] Shutdown Project"
 
 if [ -z "$CURRENT_PID" ]; then
-        echo "> 실행중 프로세스 발견 못함. 중지작업 pass"
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         echo "> kill -15 $CURRENT_PID"
         kill -15 $CURRENT_PID
@@ -28,7 +29,7 @@ fi
 echo "> [03 - E N D] Shutdown Project"
 echo "> [04 - START] Deploy Application File"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> Jar File Name : $JAR_NAME"
 
@@ -42,9 +43,9 @@ echo "> [05 - START] Startup Project"
 echo "> Jar Excute : $JAR_NAME"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,classpath:/application-aws.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-        -Dspring.profiles.active=aws \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-aws.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-aws-db.properties \
+    -Dspring.profiles.active=aws \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 echo "> [05 - E N D] Startup Project"
 
