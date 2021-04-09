b3342434dfda8a7f42dd0436631c0a85a22b89fb
Rosa <emfosvk@gmail.com>
2021-04-09 16:43:35 +0900

    AWS 무중단 배포 서비스 도입.

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,36 +1,33 @@
-#!/usr/bin/env bash
-#profile.sh 쉬고있는 profile 찾기.
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-function find_idle_profile(){
-  RESPONCE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=aws2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
 
-  if [${RESPONCE_CODE} -ge 400] #400보다 크면 에러
-  then
-    CURRENT_PROFILE=aws2
-  else
-    CURRENT_PROFILE=$(curl -s http://localhost/profile)
-  fi
+    if [ ${CURRENT_PROFILE} == aws1 ]
+    then
+      IDLE_PROFILE=aws2
+    else
+      IDLE_PROFILE=aws1
+    fi
 
-  if[${CURRENT_PROFILE} == aws1]
-  then
-    IDLE_PROFILE = aws2
-  else
-    IDLE_PROFILE = aws1
-  fi
-
-  echo "CURRENT_PROFILE : ${CURRENT_PROFILE}"
-  echo "IDLE_PROFILE    : ${IDLE_PROFILE}"
+  echo "${IDLE_PROFILE}"
 }
 
-function find_idle_port(){
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
 
-  IDLE_PROFILE = $(find_idle_profile)
-
-  if [${IDLE_PROFILE} = aws1]
-  then
-    echo "8081"
-  else
-    echo "8082"
-  fi
-
+    if [ ${IDLE_PROFILE} == aws1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
 }
